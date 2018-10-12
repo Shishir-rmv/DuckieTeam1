af6ff0406813fdafd1700afb076313c1b8b3191e
Shishir Verma <shishir.rmv@gmail.com>
2018-10-12 15:41:32 -0400

    [Shishir/Jake] Pi-Arduino interface scripts

---
+++ Pi-Duino/sketch_oct11a/sketch_oct11a.ino
@@ -0,0 +1,28 @@
+char dataString[5] = {0};
+int a = 0;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.available();
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    Serial.readBytes(dataString, 5);
+    char command[] = {dataString[0], dataString[1], dataString[2]};
+    int num1 = dataString[3] - '0';
+    int num2 = dataString[4] - '0';
+    Serial.println("ack");
+    String str(command);
+    
+    if(str == "add"){
+      Serial.println(num1 + num2);
+    }
+    Serial.flush();
+    } 
+  //a++; //
+  //sprintf(dataString, "%02X", a);
+  //Serial.println(dataString);
+ 
+  delay(1000); 
+}
