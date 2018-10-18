0a1531df7c46889f6d7ee0f216ce7e53f7ab93e8
Shishir Verma <shishir.rmv@gmail.com>
2018-10-18 14:56:36 -0400

    [Jake/Shishir] Add calculator that uses Pi-Duino Interface

--- Pi-Duino/pi_duino_2.py
+++ Pi-Duino/pi_duino_2.py
@@ -1,11 +1,15 @@
-import serial, time
+import serial, time, sys
 port = "/dev/ttyACM0"
 rate = 9600
 
 
-s1 = serial.Serial(port, rate, timeout=0)
+s1 = serial.Serial(port, rate, timeout=2)
 
-s1.write("add23")
+#time.sleep(2)
+
+send = "add" + str(sys.argv[1] ) + str(sys.argv[2])
+
+s1.write(send.encode())
 time.sleep(1)
 result = s1.readline()
-print(result)
+print(result.decode("utf-8"))
