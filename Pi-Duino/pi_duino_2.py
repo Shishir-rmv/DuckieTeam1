8a09f3ed4219da1f6cfe94d08133171f1588216a
Shishir Verma <shishir.rmv@gmail.com>
2018-10-19 16:42:11 -0400

    [Shishir] Ping code with ino folder structure

--- Pi-Duino/pi_duino_2.py
+++ Pi-Duino/pi_duino_2.py
@@ -7,9 +7,10 @@ s1 = serial.Serial(port, rate, timeout=2)
 
 #time.sleep(2)
 
-send = "add" + str(sys.argv[1] ) + str(sys.argv[2])
+send = str(sys.argv[1]) + str(sys.argv[2] ) + str(sys.argv[3])
 
 s1.write(send.encode())
 time.sleep(1)
 result = s1.readline()
 print(result.decode("utf-8"))
+
