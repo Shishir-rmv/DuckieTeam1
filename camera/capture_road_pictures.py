f884de717d23a33c0c8ffb7cb300d11b344759a5
Shishir Verma <shishir.rmv@gmail.com>
2018-10-31 16:33:03 -0400

    [Shishir/Jake] Add more road pictures

--- camera/capture_road_pictures.py
+++ camera/capture_road_pictures.py
@@ -5,7 +5,9 @@ camera = PiCamera()
 
 camera.start_preview()
 for x in range(10):
+  print("waiting for 5 seconds")
   sleep(5)
+  print("Capturing")
   camera.capture('road' + str(x) + '.jpg')
 
 camera.stop_preview()
