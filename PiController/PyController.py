431a1cc57f45d303de2f3a51fbf8c98c3567f391
JohnnyBuns <jazevedowms@gmail.com>
2018-11-06 15:18:42 -0500

    added functions for sending commands to arduino

--- PiController/PyController.py
+++ PiController/PyController.py
@@ -1,11 +1,54 @@
 from multiprocessing import Process, Value
 import serial, time
 
+#global variables
+rate = 9600
+s1 = serial.Serial(port, rate, timeout=0)
+motorL = 0 # motor speeds
+motorR = 0
+pingD = 0 #ping distance
+qeL = 0 # ir counts
+qeR = 0
+#below variables only needed if pi doing QE distance calculations
+PPR = 8
+WHEEL_BASE = 137 #current approximation in mm, chassis constant
+WHEEL_CIRCUMFERENCE = 219.9115
+
 # this will be the process that we split off for Dmitry to do computer vision work in
 # we sue shared memory to make passing information back and fourth
 def vision(distLeft, distRight, angle, go):
     while (go):
     	# Do Dmitry stuff
+
+#function to grab encoder data 
+#have to decide if the arduino will return just increments or already calculate the distance per wheel and theta itself
+def getEncoder():
+	send = 'irr'
+	s1.write(send.encode())
+	time.sleep(1)
+	
+	result = (s1.readline()).decode("utf-8")
+	qe = result.split(' ')
+	qeL = qe[0]
+	qeR = qe[1]
+
+#get ping distance
+def getPing():
+	send = 'png'
+	s1.write(send.encode())
+	time.sleep(1)
+	pingD = (s1.readline()).decode("utf-8")
+
+#stop motors
+def stop():
+	send = 'stp'
+	s1.write(send.encode())
+
+#set motor speed
+def setMotors(motorSpeedL, motorspeedR):
+	send = 'mtr' + str(motorSpeedL) + str(motorspeedR)
+	s1.write(send.encode())
+	time.sleep(1)
 
 # main method
 if __name__ == '__main__':
@@ -25,7 +68,7 @@ if __name__ == '__main__':
     # _____________________________________________________________________________________
 
 	# open the serial port to the Arduino
-	s1 = serial.Serial(port, rate, timeout=0)
+	
 	s1.flushInput()
 
 	# Do controller stuff
