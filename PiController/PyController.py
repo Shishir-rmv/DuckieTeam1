af20d12d15f9d0dd6c0326f55523852280e46b00
JDowns <jmdz412@gmail.com>
2018-11-07 13:30:21 -0500

    controller updates

--- PiController/PyController.py
+++ PiController/PyController.py
@@ -9,10 +9,12 @@ motorR = 0
 pingD = 0 #ping distance
 qeL = 0 # ir counts
 qeR = 0
+timeoutSecs = 1
 #below variables only needed if pi doing QE distance calculations
 PPR = 8
 WHEEL_BASE = 137 #current approximation in mm, chassis constant
 WHEEL_CIRCUMFERENCE = 219.9115
+
 
 # this will be the process that we split off for Dmitry to do computer vision work in
 # we sue shared memory to make passing information back and fourth
@@ -20,39 +22,51 @@ def vision(distLeft, distRight, angle, go):
     while (go):
     	# Do Dmitry stuff
 
+
 #function to grab encoder data 
 #have to decide if the arduino will return just increments or already calculate the distance per wheel and theta itself
 def getEncoder():
 	send = 'irr'
 	s1.write(send.encode())
-	time.sleep(1)
-	
-	result = (s1.readline()).decode("utf-8")
-	qe = result.split(' ')
-	qeL = qe[0]
-	qeR = qe[1]
+	response = (s1.readline(timeout=timeoutSecs)).decode("utf-8")
+
+	if (!response):
+		print ("No result received from Arduino on getEncoder call")
+	else
+		qe = response.split(' ')
+		qeL = qe[0]
+		qeR = qe[1]
+
 
 #get ping distance
 def getPing():
 	send = 'png'
 	s1.write(send.encode())
-	time.sleep(1)
-	pingD = (s1.readline()).decode("utf-8")
+	response = s1.readline(timeout=timeoutSecs).decode("utf-8")
+
+	if (!response):
+		print ("No result received from Arduino on getPing call")
+	else
+		pingD = response
+
 
 #stop motors
 def stop():
 	send = 'stp'
 	s1.write(send.encode())
 
+
 #set motor speed
 def setMotors(motorSpeedL, motorspeedR):
 	send = 'mtr' + str(motorSpeedL) + str(motorspeedR)
 	s1.write(send.encode())
-	time.sleep(1)
 
-# main method
-if __name__ == '__main__':
+	#update the global variables once they're written to serial
+	motorL = motorSpeedL
+	motorR = motorSpeedR
 
+
+def runController():
 	# Define and split off the computer vision subprocess _________________________________
 	# define doubles
 	distLeft = Value('d', -9.99)
@@ -68,15 +82,24 @@ if __name__ == '__main__':
     # _____________________________________________________________________________________
 
 	# open the serial port to the Arduino
-	
 	s1.flushInput()
 
 	# Do controller stuff
-	s = [0]
+	response = ""
+
+	count = 0
+	running = True
 
 	if s1.isOpen():
 		s1.flush()
-		print("starting")
+		print("PyController starting")
+		
+		while (running):
+			#check distance to lines on either side & angle in lane
+			#compute wheel speed adjustments based off of current speed and required corrections
+			#set wheels to corrected speed
+			#consider sleeping until a timeDelta has passed?
+
 		s1.readline()
 		for x in range(10):
 			s1.write(b'add23')
@@ -88,8 +111,19 @@ if __name__ == '__main__':
 			print("x:" + str(x))
 			print("Computing: " + s[0])
 
+	#stop vehicle process. Set motor speeds to 0, close down serial port, and kill vision thread.
+	setMotors(0,0)
 	s1.close()
-
-	# once we're all done, send the kill switch and join the vision process
+	# once we're all done, send the kill switch to the inner vision loop and join the vision process
     go.value = False
     p.join()
+
+
+def runTracker():
+
+
+# main method
+if __name__ == '__main__':
+
+	#runController()
+	runTracker()
