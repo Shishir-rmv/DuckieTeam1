85b8024179501d23502a7a606322b27414b24027
Shishir Verma <shishir.rmv@gmail.com>
2018-11-11 12:28:08 -0500

    Add more images

--- peripherals/camera/camera2.py
+++ peripherals/camera/camera2.py
@@ -1,9 +1,9 @@
 from picamera import PiCamera
 from time import sleep
-
+import sys
 camera = PiCamera()
 
 camera.start_preview()
 sleep(5)
-camera.capture('image2.jpg')
+camera.capture(sys.argv[1])
 camera.stop_preview()
