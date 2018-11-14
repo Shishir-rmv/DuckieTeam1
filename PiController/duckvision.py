63967c303472e2e91ec43f03cafcd6b15b192426
JDowns <jmdz412@gmail.com>
2018-11-14 17:51:07 -0500

    vision bug fixes

--- PiController/duckvision.py
+++ PiController/duckvision.py
@@ -119,6 +119,10 @@ def select_yellow(image):
 
 
 def process(stream, vOffset):
+    # Jake needed to initialize these since they were drawing errors
+    white_line_x1, white_line_y1, white_line_x2, white_line_y2 = 0,0,0,0
+    yellow_line_x1, yellow_line_y1, yellow_line_x2, yellow_line_y2 = 0,0,0,0
+
     try:
         stream.seek(0)  # seek to location 0 of stream_img
         # Truncate the stream to the current position (in case
