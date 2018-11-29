6fdf3bf5592b468f85e48cdabfd6e1b7a36e6b98
Shishir Verma <shishir.rmv@gmail.com>
2018-11-29 17:45:43 -0500

    jake is being lazy with an auto-push

--- peripherals/camera/camera.py
+++ peripherals/camera/camera.py
@@ -2,7 +2,7 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-camera.resolution = (1920, 1080)
+camera.resolution = (1024, 768)
 camera.start_preview()
 #sleep(5)
 camera.capture('image.jpg')
