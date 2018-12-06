36094ca549972cb8211601e41f6bcd04a7ca8a6d
Shishir Rajmohan Verma <shishir.rmv@gmail.com>
2018-12-06 15:52:07 -0500

    [Shishir] Update Vision

--- PiController/PyController.py
+++ PiController/PyController.py
@@ -245,8 +245,8 @@ def visionController():
     starter_thread = threading.Thread(target=starter)
     starter_thread.start()
 
-    serial_thread = threading.Thread(target=serialReader)
-    serial_thread.start()
+    # serial_thread = threading.Thread(target=serialReader)
+    # serial_thread.start()
 
     # open the serial port to the Arduino & initialize
     s1.flushInput()
@@ -303,7 +303,7 @@ def visionController():
     # join the starter and serial threads, kill vision
     starter_thread.join()
     print("Starter thread joined")
-    serial_thread.terminate()
+    # serial_thread.terminate()
     print("Serial thread joined")
     vision_process.terminate() 
     print("Vision process terminated")
