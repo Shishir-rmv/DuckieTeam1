2c4ae22692d9f2daa57f5d04a66e30611998d128
JDowns <jmdz412@gmail.com>
2018-12-06 21:34:06 -0500

    jake is being lazy with an auto-push

--- PiController/PyController.py
+++ PiController/PyController.py
@@ -261,7 +261,7 @@ def visionController():
     running, stateChange, odometry, flag = True, False, True, True
 
     # in mm/sec
-    vRef = 0
+    vRef = 50
 
     if s1.isOpen():
         s1.flush()
