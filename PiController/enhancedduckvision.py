e289a3f508fdaeb80ee3bf5645026f7667c24ce2
Shishir Rajmohan Verma <shishir.rmv@gmail.com>
2018-12-07 15:10:33 -0500

    [Shishir] BugFix

--- PiController/enhancedduckvision.py
+++ PiController/enhancedduckvision.py
@@ -102,7 +102,7 @@ def process(stream, vOffset):
                         int(yellow_px[0]), current_center, diff))
                     vOffset.value = int(diff)
                 elif white_exist and not yellow_exist:
-                    diff = 1100 - int(white_px[1])
+                    diff = int(white_px[1]) - 525
                     vOffset.value = int(diff)
                     print("%s\tWhite Pixel: x = %d, y = %d\t diff: %d" % (datetime.datetime.now(), int(white_px[1]), int(white_px[0]), diff))
                 elif yellow_exist and not white_exist:
