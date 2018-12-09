056a12574f12e6badcb56fd11922f0de9d6b6b61
Shishir Verma <shishir.rmv@gmail.com>
2018-12-09 15:28:36 -0500

    [Shishir] Adding picture of bad corner

--- peripherals/camera/camera2.py
+++ peripherals/camera/camera2.py
@@ -2,7 +2,7 @@ from picamera import PiCamera
 from time import sleep
 import sys
 camera = PiCamera()
-camera.resolution = (1280, 720)
+camera.resolution = (640, 480)
 camera.start_preview()
 sleep(1)
 camera.capture(sys.argv[1])
