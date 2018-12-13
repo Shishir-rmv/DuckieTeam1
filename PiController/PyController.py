fa54437d2c6472617f27bb5ff98ef63048e5f974
Shishir Rajmohan Verma <shishir.rmv@gmail.com>
2018-12-13 16:00:21 -0500

    [Shishir] Debug statements

--- PiController/PyController.py
+++ PiController/PyController.py
@@ -315,6 +315,7 @@ def visionController():
     try:
         # if(True):
         while (running):
+            print("stopped: %r \t greenLight: %r \t stopLine: %r \t laststart: %s" % (stopped, greenLight.value, stopLine.value, lastStart))
             # only do this if we have changed state in our state machine?
 
             # if the starter thread has changed this global variable to allow movement
@@ -326,7 +327,7 @@ def visionController():
                 #     write("srt0000%s\n" % str(vRef).zfill(4))
                 #     # print("Finished writing start")
                 #     flag = False
-                print("stopped: %r \t greenLight: %r \t stopLine: %r" % (stopped, greenLight, stopLine))
+
                 if (stopLine.value and not stopped and (datetime.now() - lastStart).seconds > 2):
                     print("Stopper middle: stp")
                     write("stp")
