0f0e2260d93e37e7479d288adef8d39ff84b7a21
JDowns <jmdz412@gmail.com>
2018-12-14 16:54:06 -0500

    jake is being lazy with an auto-push

--- PiController/PyController.py
+++ PiController/PyController.py
@@ -483,7 +483,7 @@ def runController():
             for currentState in range(len(route) - 1):
                 # by performing all of the actions in the route
                 # when the last action is completed, the next state will happen in the parent for loop
-                pdb.set_trace()
+                # pdb.set_trace()
                 actionMap = edges[str(route[currentState]) + "," + str(route[currentState+1])]["attrs"]["map"]['actions']
 
                 # debugging
